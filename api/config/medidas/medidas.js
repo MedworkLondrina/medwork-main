8f53049d8282590d894e3db242b9e3884f247c8e
Medwork <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>
2024-05-24 16:24:58 -0300

    arrumando medidas

--- api/config/medidas/medidas.js
+++ api/config/medidas/medidas.js
@@ -2,41 +2,44 @@ import { pool } from "../../db.js";
 
 function getMedidasFromTabela(tenantCode, grupo) {
     return new Promise((resolve, reject) => {
-        // Primeiro, verifica o valor de 'global' para o tenant_code especificado
-        const queryGlobalCheck = 'SELECT global FROM tenant WHERE tenant_code =?';
+        const queryGlobalCheck = 'SELECT global FROM tenant WHERE tenant_code = ?';
         pool.getConnection((err, con) => {
             if (err) {
                 reject(err);
                 return;
             }
             con.query(queryGlobalCheck, [tenantCode], (err, results) => {
-                con.release();
                 if (err) {
+                    con.release();
                     reject(err);
                     return;
                 }
-                // Verifica se results é um array e contém pelo menos um elemento
+
                 if (Array.isArray(results) && results.length > 0) {
-                    const [global] = results;
+                    const global = results[0].global;
                     let query;
                     let queryParams = [];
-                    if (global.global === 1) {
+
+                    if (global === 1) {
                         if (grupo === 'all') {
-                            query = 'SELECT * FROM medidas';
+                            query = 'SELECT * FROM medidas WHERE tenant_code = ? OR tenant_code IS NULL';
+                            queryParams.push(tenantCode);
                         } else {
-                            query = 'SELECT * FROM medidas WHERE grupo_medida =?';
-                            queryParams = [grupo];
+                            query = 'SELECT * FROM medidas WHERE (tenant_code = ? OR tenant_code IS NULL) AND grupo_medida = ?';
+                            queryParams.push(tenantCode, grupo);
                         }
                     } else {
                         if (grupo === 'all') {
-                            query = 'SELECT * FROM medidas WHERE tenant_code =?';
+                            query = 'SELECT * FROM medidas WHERE tenant_code = ?';
+                            queryParams.push(tenantCode);
                         } else {
-                            query = 'SELECT * FROM medidas WHERE tenant_code =? AND grupo_medida =?';
-                            queryParams = [tenantCode, grupo];
+                            query = 'SELECT * FROM medidas WHERE tenant_code = ? AND grupo_medida = ?';
+                            queryParams.push(tenantCode, grupo);
                         }
                     }
-                    // Executa a consulta final com base na lógica acima
+
                     con.query(query, queryParams, (err, data) => {
+                        con.release();
                         if (err) {
                             reject(err);
                             return;
@@ -44,8 +47,8 @@ function getMedidasFromTabela(tenantCode, grupo) {
                         resolve(data);
                     });
                 } else {
-                    // Trata o caso em que results está vazio ou não é um array
-                    reject(new Error('No results found or results is not an array'));
+                    con.release();
+                    reject(new Error('No results found for the specified tenantCode.'));
                 }
             });
         });
